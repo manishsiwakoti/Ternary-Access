430ca727ffbb4d1debfe05537a501651d1da3c45
max-student <max-student@maxtrain.com>
2020-02-11 11:55:06 -0500

    Add project files.

---
+++ SalesLibrary/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ternary_Access
+    {
+    public class Order
+        {
+        private static int LastId = 0;
+
+        static Order()
+            {
+            LastId = 0;
+            }
+
+        public Order()
+            {
+            this.Id = LastId += 7;
+            }
+
+        public int Id { get; private set; }
+        public double Amount { get; set; }
+        public Customer Customer { get; set; }
+
+        public void SetCustomer(Customer customer)
+            {
+           
+
+            }
+        }
+    }
---
+++ Ternary-Access/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ternary_Access
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+
+            Console.WriteLine($"The cube of 17 is {Program.Cubed(17)}");
+            var message = Customer.printNextId();
+            Console.WriteLine(message);
+
+            var cust1 = new Customer("Max Technical Training");
+            Console.WriteLine(cust1);
+
+            var cust2 = new Customer("Amazon");
+            var cust3 = new Customer("Target");
+
+            cust1.NationalAccount("yes");
+
+            var order1 = new Order { Amount = 1000, Customer = cust1 };
+            var order2 = new Order { Amount = 2000, Customer = cust2 };
+            var order3 = new Order { Amount = 3000, Customer = cust3 };
+            }
+            
+
+        static int Cubed(int nbr)
+            {
+            return nbr * nbr * nbr;
+            }
+        }
+
+    }
